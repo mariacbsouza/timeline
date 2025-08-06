065dd98623708c4033405a6c86602963e67e27e2
mariacbsouza <clarabsouza@hotmail.com>
2025-08-06 13:58:44 -0300

    feat: add global style and font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import timelineItems from "./timelineItems.js";
+import "./global.css";
 
 function App() {
   return <div></div>;
