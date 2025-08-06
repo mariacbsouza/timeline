9fdccabdf3741402c8a07464a8611c4c32998086
mariacbsouza <clarabsouza@hotmail.com>
2025-08-06 13:42:30 -0300

    feat: init project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import timelineItems from "./timelineItems.js";
+
+function App() {
+  return <div></div>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
